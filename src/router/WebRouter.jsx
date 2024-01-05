d12af3c0aeae4b68b064f8816a28676615756e3a
draquio <draquio@gmail.com>
2024-01-05 00:28:22 -0400

    cambiando cositas

--- src/router/WebRouter.jsx
+++ src/router/WebRouter.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "../layout/Layout";
-import { Home } from "../pages/home";
+import { Home } from "../pages/Home";
 export const WebRouter = () => {
   const loadLayaout = (Layout, Page) => {
     return (
